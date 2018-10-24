cb1cc113cb19224e95a564ecce15c1ed8a0de87d
crashdemons <crashenator+gh@gmail.com>
2018-10-24 18:47:35 -0500

    initial codebase

---
+++ src/main/java/com/github/crashdemons/wgtrophyflags/MTListener.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.crashdemons.wgtrophyflags;
+
+import com.github.crashdemons.miningtrophies.events.BlockDropTrophyEvent;
+import static com.github.crashdemons.wgtrophyflags.WGTrophyFlagsPlugin.FLAG_MININGTROPHY;
+import static com.github.crashdemons.wgtrophyflags.WGTrophyFlagsPlugin.FLAG_TROPHYWARNINGS;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ *
+ * @author crashdemons <crashenator at gmail.com>
+ */
+public class MTListener implements Listener{
+    private final WGTrophyFlagsPlugin plugin;
+    MTListener(WGTrophyFlagsPlugin plugin){
+        this.plugin=plugin;
+    }
+    
+    @EventHandler
+    public void onBlockDropTrophyEvent(BlockDropTrophyEvent event){
+        if(!plugin.hasMiningtrophies()) return;
+        if(!plugin.isEnabled()) return;
+        if(!(event instanceof BlockDropTrophyEvent)) return;
+        BlockDropTrophyEvent trophyevent = (BlockDropTrophyEvent) event;
+        Block block = trophyevent.getBlock();
+        ItemStack reward = trophyevent.getDrop();
+        Player rewardee = trophyevent.getPlayer();
+        Location loc = block.getLocation();
+        
+        LocalPlayer wgRewardee = null;
+        if(rewardee!=null){
+            wgRewardee = plugin.getWorldGuardPlugin().wrapPlayer(rewardee);
+        }
+        com.sk89q.worldedit.util.Location wgLoc = BukkitAdapter.adapt(loc);
+        RegionQuery query = plugin.getWorldGuard().getPlatform().getRegionContainer().createQuery();
+        StateFlag.State state = query.queryState(wgLoc, wgRewardee, FLAG_MININGTROPHY);
+        StateFlag.State warningstate = query.queryState(wgLoc, null, FLAG_TROPHYWARNINGS);
+        if(state==StateFlag.State.DENY){
+            if(rewardee!=null){
+                if(warningstate!=StateFlag.State.DENY) rewardee.sendMessage(ChatColor.RED+"Hey! "+ChatColor.GRAY+"Sorry, but you can't get mining-trophies here.");
+                trophyevent.setCancelled(true);
+            }
+        }
+    }
+    
+}
---
+++ src/main/java/com/github/crashdemons/wgtrophyflags/WGTrophyFlagsPlugin.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.crashdemons.wgtrophyflags;
+
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.shininet.bukkit.playerheads.events.LivingEntityDropHeadEvent;
+
+/**
+ *
+ * @author crash
+ */
+public class WGTrophyFlagsPlugin extends JavaPlugin implements Listener {
+    WorldGuardPlugin wgp = null;
+    WorldGuard wg = null;
+    LivingEntityDropHeadEvent event;
+    
+    private boolean PHEnabled=false;
+    private boolean MTEnabled=false;
+    
+    
+    public static final StateFlag FLAG_BEHEADTROPHY = new StateFlag("behead-trophy", true);
+    public static final StateFlag FLAG_MININGTROPHY = new StateFlag("mining-trophy", true);
+    public static final StateFlag FLAG_TROPHYWARNINGS = new StateFlag("trophy-warnings", true);
+    
+    
+    
+    public boolean hasPlayerheads(){return PHEnabled;}
+    public boolean hasMiningtrophies(){return MTEnabled;}
+    public WorldGuard getWorldGuard(){ return wg; }
+    public WorldGuardPlugin getWorldGuardPlugin(){ return wgp; }
+    
+    private WorldGuardPlugin findWorldGuardPlugin() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+
+        // WorldGuard may not be loaded
+        if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+            return null; // Maybe you want throw an exception instead
+        }
+
+        return (WorldGuardPlugin) plugin;
+    }
+    
+    private boolean isPHAvailable(){
+        if(this.getServer().getPluginManager().getPlugin("PlayerHeads") != null){
+            return classExists("org.shininet.bukkit.playerheads.events.MobDropHeadEvent");
+        }
+        return false;
+    }
+    private boolean isMTAvailable(){
+        if(this.getServer().getPluginManager().getPlugin("MiningTrophies") != null){
+            return classExists("com.github.crashdemons.miningtrophies.events.BlockDropTrophyEvent");
+        }
+        return false;
+    }
+    
+    
+    private boolean classExists(String name){
+        try {
+            Class.forName( name );
+            return true;
+        } catch( ClassNotFoundException e ) {
+            return false;
+        }
+    }
+    
+    private boolean wgInit(){
+        wgp = findWorldGuardPlugin();
+        wg = WorldGuard.getInstance();
+        if(wgp==null || wg==null){
+            return false; 
+        }
+        
+        FlagRegistry registry = wg.getFlagRegistry();
+        try {
+            // register our flag with the registry
+            registry.register(FLAG_BEHEADTROPHY);
+            registry.register(FLAG_MININGTROPHY);
+            registry.register(FLAG_TROPHYWARNINGS);
+            return true;
+        } catch (FlagConflictException e) {
+            // some other plugin registered a flag by the same name already.
+            // you may want to re-register with a different name, but this
+            // could cause issues with saved flags in region files. it's better
+            // to print a message to let the server admin know of the conflict
+            getLogger().severe("Could not register WG flags due to a conflict with another plugin");
+            return false;
+        }
+    }
+    
+    private boolean pluginInit(){
+        this.PHEnabled=isPHAvailable();
+        this.MTEnabled=isMTAvailable();
+        
+        if(PHEnabled || MTEnabled){
+            if(PHEnabled) getLogger().info("PlayerHeads support detected");
+            if(MTEnabled) getLogger().info("MiningTrophies support detected");
+            return true;
+        }else{
+            getLogger().warning("Neither PlayerHeads or MiningTrophies plugins are present - disabling plugin");
+            this.getServer().getPluginManager().disablePlugin(this);
+            return false;
+        }
+    }
+    
+    @Override
+    public void onLoad(){
+        if(!wgInit()) return;
+    }
+    
+    @Override
+    public void onEnable(){
+        getLogger().info("Enabling...");
+        if(!pluginInit()) return;
+        if(PHEnabled){
+            getServer().getPluginManager().registerEvents(new PHListener(this), this);
+        }
+        if(MTEnabled){
+            getServer().getPluginManager().registerEvents(new MTListener(this), this);
+        }
+        getLogger().info("Enabled.");
+    }
+    
+    @Override
+    public void onDisable(){
+        getLogger().info("Disabling...");
+        getLogger().info("Disabled.");
+    }
+
+}
